d0d315ec340f7c5b14998c2ac674f668b7ec2f94
SofaLahnova <90931727+SofaLahnova@users.noreply.github.com>
2023-02-02 22:58:37 +0400

    Add files via upload

---
+++ dagster_site.py
@@ -0,0 +1,17 @@
+import pandas as pd
+from dagster import asset, get_dagster_logger
+from sklearn.model_selection import train_test_split
+from catboost import Pool, CatBoostRegressor
+from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
+
+@asset
+def df_site() -> None: 
+# assign data of lists. 
+    df = pd.read_csv("df_site.csv")
+    print(df)
+#    data = {'name': ['hello', 'world'], 'url': ["http://hello.com/home", "https://world.org/"]}
+# Create DataFrame
+#    df = pd.DataFrame(data)
+    df['domain'] = df['url'].map(lambda x: str(x).partition("://")[2].partition("/")[0])
+    print(df)
+    df.to_csv('df_new_site.csv')
